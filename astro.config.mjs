559ecb8928cb95946ef654ac8803de9ddadfac8b
David Jaimes <dj@davidjaimes.com>
2024-03-16 18:58:31 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'davidjaimes.com',
+  site: 'https://davidjaimes.com',
   integrations: [
     expressiveCode({
       themes: ['github-dark-dimmed'],
