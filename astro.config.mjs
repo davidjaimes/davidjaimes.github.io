d72c8a810e64df23b81ded9a8ce49a8ed5bbaa00
David Jaimes <github@davidjaimes.com>
2025-06-06 22:22:58 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,45 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://davidjaimes.com/',
+  site: "https://davidjaimes.com/",
   integrations: [
     expressiveCode({
-      themes: ['github-dark-dimmed'],
+      themes: ["github-dark-dimmed"],
       styleOverrides: {
-        borderColor: '#0d1117',
-        borderWidth: '3px',
+        borderColor: "#0d1117",
+        borderWidth: "3px",
         frames: {
-          shadowColor: 'transparent',
+          shadowColor: "transparent",
 
-          terminalTitlebarBackground: '#161b22',
-          terminalTitlebarBorderBottomColor: '#0d1117',
+          terminalTitlebarBackground: "#161b22",
+          terminalTitlebarBorderBottomColor: "#0d1117",
 
           //editorActiveTabBackground: '#161b22',
-          editorTabBarBackground: '#161b22',
-          editorTabBarBorderBottomColor: '#0d1117',
-          editorActiveTabIndicatorTopColor: 'transparent',
-          editorActiveTabIndicatorBottomColor: '#0d1117',
-          terminalTitlebarDotsForeground: '#0d1117',
-          terminalTitlebarDotsOpacity: '1.0',
+          editorTabBarBackground: "#161b22",
+          editorTabBarBorderBottomColor: "#0d1117",
+          editorActiveTabIndicatorTopColor: "transparent",
+          editorActiveTabIndicatorBottomColor: "#0d1117",
+          terminalTitlebarDotsForeground: "#0d1117",
+          terminalTitlebarDotsOpacity: "1.0",
 
-          inlineButtonBorder: 'transparent',
-
-        }
-      }
+          inlineButtonBorder: "transparent",
+        },
+      },
     }),
     tailwind(),
-    react()
-  ]
+    react(),
+  ],
+  experimental: {
+    fonts: [
+      {
+        name: "Inter",
+        cssVariable: "--font-inter",
+        provider: fontProviders.fontsource(),
+      },
+    ],
+  },
 });
