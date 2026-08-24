a64949f23507df385dca91dc8671dd745197aadf
David Jaimes <github@davidjaimes.com>
2026-08-23 21:07:43 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://davidjaimes.github.io',
+  base: '/davidjaimes.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
